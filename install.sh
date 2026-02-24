db6845ca1b8cfba16881d93197ead42e6ca769e3
originalbluef3x <66074938+originalbluef3x@users.noreply.github.com>
2026-02-23 21:35:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -419,7 +419,7 @@ install_standalone() {
   "$sh_c" tar -C "$STANDALONE_INSTALL_PREFIX/lib" -xzf "$CACHE_DIR/code-server-$VERSION-$OS-$ARCH.tar.gz"
   "$sh_c" mv -f "$STANDALONE_INSTALL_PREFIX/lib/code-server-$VERSION-$OS-$ARCH" "$STANDALONE_INSTALL_PREFIX/lib/code-server-$VERSION"
   "$sh_c" ln -fs "$STANDALONE_INSTALL_PREFIX/lib/code-server-$VERSION/bin/code-server" "$STANDALONE_INSTALL_PREFIX/bin/code-server"
-
+  "$sh_c" ln -sfn "$STANDALONE_INSTALL_PREFIX/lib/code-server-$VERSION" "$STANDALONE_INSTALL_PREFIX/lib/code-server-current"
   echo_standalone_postinstall
 }
 
